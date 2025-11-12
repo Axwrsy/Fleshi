09a2fc8dcd2afe79293fbc6234f23f5b09b69e2c
ana <ana.c.paula11@aluno.senai.br>
2025-11-12 14:44:14 -0300

    feat: add homepage and profile pages

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bem vindo ao Fléshi!'
+    return render_template('homepage.html')
+
+#rota dinamica nome da var
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', username=username)
+
+
+
 
 
 
