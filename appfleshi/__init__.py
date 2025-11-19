4c90449e00cf066a896cf59b9aa41332a4dbafb0
Aluno <ana.c.paula11@aluno.senai.br>
2025-11-19 14:47:13 -0300

    feat: login and security system setup

--- appfleshi/__init__.py
+++ appfleshi/__init__.py
@@ -1,13 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+#libs p login
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
 app = Flask(__name__)
 
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datafleshi.db"
+app.config['SECRET_KEY'] = 'secret'
+
+
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+
+#direciona p onde está a tela de login (homepage no caso)
+login_manager.login_view = 'homepage'
 
 
 
