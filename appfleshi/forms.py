71394ddf455775bfb02041b6c52fcddfef69f225
Aluno <ana.c.paula11@aluno.senai.br>
2025-11-19 16:02:48 -0300

    feat: add class LoginForm and RegisterForm

--- appfleshi/forms.py
+++ appfleshi/forms.py
@@ -11,11 +11,11 @@ class LoginForm(FlaskForm):
 
 #criando forms - cadastro
 class RegisterForm(FlaskForm):
-   email = StringField('Email', validators=[DataRequired(), Email()]) #length p definir a qntd de caracteres
-   username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=4, max=20)])
-   password = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=60)])
-   confirm_password = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('password')]) #entende q tem q ta igual a password p funcionar
-   submit = SubmitField('Criar Conta')
+    email = StringField('Email', validators=[DataRequired(), Email()]) #length p definir a qntd de caracteres
+    username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=60)])
+    confirm_password = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('password')]) #entende q tem q ta igual a password p funcionar
+    submit = SubmitField('Criar Conta')
 
     #função p ver se o email q digitou já existe
     def validate_email(self, email):
