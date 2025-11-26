e86bb9f22a00bf5ccfd6f416b71e6e0a6d3e8561
Aluno <ana.c.paula11@aluno.senai.br>
2025-11-26 16:54:22 -0300

    feat: add create account

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,8 +1,9 @@
-from flask import  render_template, request
-from flask_login import login_required
+from flask import  render_template, url_for, redirect
+from flask_login import login_required, login_user, logout_user, current_user
 
-from appfleshi import app
+from appfleshi import app, database, bcrypt
 from appfleshi.forms import LoginForm, RegisterForm
+from appfleshi.models import User, Photo
 
 #redireciona p login
 @app.route('/')
@@ -14,6 +15,13 @@ def homepage():
 @app.route('/createaccount', methods=['GET', 'POST'])
 def createaccount():
     register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        password = bcrypt.generate_password_hash(register_form.password.data) #criptografa a senha
+        user = User(username=register_form.username.data, email=register_form.email.data, password=password)#p nao armazenaar a senha pura
+        database.session.add(user)
+        database.session.commit()
+        login_user(user, remember=True) #faz o login do user
+        return redirect(url_for('profile', username=user.username)) #retorna pra função
     return render_template('createaccount.html', form=register_form)
 
 
