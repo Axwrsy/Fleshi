6c9202a331b482c54e1eec3ca9485b139d88361a
carol <annasantos4530@gmail.com>
2025-12-12 09:37:26 -0300

    feat: add bio and bootstrap

---
+++ forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm #criar forms
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length #importa todas partes do forms
+from appfleshi.models import User #importa a tabela p conseguir ver os users
+
+
+#em caso de legenda mexer aqq
+class PhotoForm(FlaskForm):
+    photo = FileField('Foto', validators=[DataRequired()])
+    submit = SubmitField('Postar')
+
+
+
+
+#criando forms - login
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+    #ver se o email nao esta cadastrado no banco
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if not user:
+           raise ValidationError("Usuário não encontrado")
+        return None
+
+
+
+#criando forms - cadastro
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()]) #length p definir a qntd de caracteres
+    username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=60)])
+    confirm_password = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('password')]) #entende q tem q ta igual a password p funcionar
+    submit = SubmitField('Criar Conta')
+
+    #função p ver se o email q digitou já existe
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+           raise ValidationError("Email já cadastrado. Faça Login!")
+        return None
+
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+           raise ValidationError("Nome de usuário já existe. Escolha outro")
+        return None
+
+
